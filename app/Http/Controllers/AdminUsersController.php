c8e73e2feb0d0bb6a9a9c3f67152fc5f2e5fc0e3
Patricia <ptregarde@gmail.com>
2018-11-19 10:26:40 +0800

    Added photo input

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\User;
 use App\Role;
+use App\Photo;
 use App\Http\Requests\UsersRequest;
 
 class AdminUsersController extends Controller
@@ -40,9 +41,23 @@ class AdminUsersController extends Controller
      */
     public function store(UsersRequest $request)
     {
-        User::create($request->all());
 
-        return redirect('/admin/users');
+        $input = $request->all();
+
+        if($file = $request->file('photo_id')){
+            $name = time() . $file->getClientOriginalName();
+
+            $file->move('images', $name);
+
+            $photo = Photo::create(['file'=>$name]);
+
+            $input['photo_id'] = $photo->id;        
+        }
+
+        $input['password'] = bcrypt($request->password);
+        User::create($input);
+
+        // return redirect('/admin/users');
         // return $request->all();
     }
 
--- database/migrations/2018_11_19_012946_add_photo_id_to_users.php
+++ database/migrations/2018_11_19_012946_add_photo_id_to_users.php
@@ -14,7 +14,7 @@ class AddPhotoIdToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('photo_id')->nullable();
+            $table->integer('photo_id')->nullable();
         });
     }
 
